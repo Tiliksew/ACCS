0bc1a52d726d1b000263beebf8784a5e7edde44e
Tiliksew <tiliksewmulugeta552@gmail.com>
2022-02-07 22:03:06 +0300

    the last import command is removed

--- Home.java
+++ Home.java
@@ -13,7 +13,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
+
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
